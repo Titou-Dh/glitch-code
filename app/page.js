0f8ab6a4e10cbdbd37aff939bfcbe9788d7380a1
Titou-Dh <titoudh1@gmail.com>
2024-02-25 02:24:16 +0100

    modified the component

--- app/page.js
+++ app/page.js
@@ -1,46 +1,20 @@
 "use client"
 import React from 'react';
-import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import Countdown from '../components/Countdown';
 
-const CountdownPage = () => {
-  const eventTime = new Date(); // Initialize with current date
-  eventTime.setHours(23); // Set the hour (0-23)
-  eventTime.setMinutes(59); // Set the minute (0-59)
-  eventTime.setSeconds(0); // Set the second (0-59)
+const App = () => {
 
-  const calculateTimeLeft = () => {
-    const now = new Date();
-    const difference = eventTime - now;
-    let timeLeft = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
-
-    return timeLeft;
-  };
+  const targetTime = new Date();
+  targetTime.setUTCHours(17);
+  targetTime.setUTCMinutes(30);
+  targetTime.setUTCSeconds(0);
 
   return (
-    <div className="container">
-      <h1>Download Countdown App</h1>
-      <div className="countdown-wrapper">
-        <CountdownCircleTimer
-          isPlaying
-          duration={eventTime - new Date()}
-          colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}
-          onComplete={() => [<span>Event has started!</span>]}
-        >
-          {({ remainingTime }) => remainingTime}
-        </CountdownCircleTimer>
-      </div>
-      <button>Download File</button>
+    <div className="App">
+      <h1>Countdown Before the Glitch hackathon</h1>
+      <Countdown targetTime={targetTime} />
     </div>
   );
 };
 
-export default CountdownPage;
+export default App;
---
+++ components/Countdown.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const Countdown = ({ targetTime }) => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetTime) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const formatTime = (value) => {
+    return value < 10 ? `0${value}` : value;
+  };
+
+  return (
+    <div className="countdown">
+        {`${formatTime(timeLeft.hours)}:${formatTime(timeLeft.minutes)}:${formatTime(timeLeft.seconds)}`}
+    </div>
+  );
+};
+
+export default Countdown;
